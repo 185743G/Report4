684bfe0db47ec091bd3499131bf43711d9932a0b
Yusuke OSIRO <e185743@ie.u-ryukyu.ac.jp>
2019-02-14 12:37:19 +0900

    report4, Step3 third commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
-    public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e) {
-            System.out.println(e.getMessage());
-            System.out.println(e+"を検出しました");
-        }
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 }
+//        try{
+//            String str = null;
+//            System.out.println(str.length());
+//        }catch(NullPointerException e) {
+//            System.out.println(e.getMessage());
+//            System.out.println(e+"を検出しました");
+//        }
